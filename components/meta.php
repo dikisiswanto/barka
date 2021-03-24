16784ba68dfe6cee6eec667759084744b782fbe9
Diki Siswanto <dickyachmady@gmail.com>
2021-03-25 07:51:55 +0800

    Update meta.php

--- components/meta.php
+++ components/meta.php
@@ -3,6 +3,7 @@
 
 <title><?=isset($page_title) ? $page_title . ' | ' : ''?><?=$this->session->school_name?></title>
 <meta charset="utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <meta http-equiv="X-UA-Compatible" content="IE=edge" />
 <meta name="keywords" content="<?=$this->session->meta_keywords;?>"/>
