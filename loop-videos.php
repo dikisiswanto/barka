b6a30bb35b1507a9bb6841596f4358cb46392e3c
Diki Siswanto <dickyachmady@gmail.com>
2021-05-08 13:21:03 +0800

    fix error

--- loop-videos.php
+++ loop-videos.php
@@ -8,6 +8,20 @@
     }
   });
 
+  function setLoading(state) {
+    const loading = `
+      <div class="fixed top-0 right-0 left-0 bottom-0 w-full min-h-screen bg-black bg-opacity-20 flex items-center justify-center text-white z-50 flex-col">
+        <i class="fa fa-spinner fa-spin text-5xl" aria-label="Loading..."></i>
+        <span>Memuat...</span>
+      </div>
+    `
+    if (state) {
+      $('.loading-area').html(loading);
+    } else {
+      $('.loading-area').html('')
+    }
+  }
+
   function get_videos() {
     page++;
     const data = {
