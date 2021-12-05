f010dca8ff976fcc5786d9bba4b2edf209e24898
Diki Siswanto <siswantod16d@student.unhas.ac.id>
2021-12-05 17:41:35 +0800

    Update sidebar.php

--- components/sidebar.php
+++ components/sidebar.php
@@ -48,6 +48,7 @@
   <div class="bg-white space-y-3 py-3">
     <h5 class="text-title font-bold font-heading text-lg lg:text-xl px-4">Tautan</h5>
     <div class="px-4 divide-y">
+      <a href="https://www.trivusi.web.id" title="Trivusi.web.id" target="_blank" class="block py-2 focus:text-secondary">Trivusi.web.id</a>
       <?php foreach($links->result() as $link) : ?>
         <a href="<?= $link->link_url ?>" title="<?= $link->link_title ?>" target="<?= $link->link_target ?>" class="block py-2 focus:text-secondary"><?= $link->link_title ?></a>
       <?php endforeach ?>
