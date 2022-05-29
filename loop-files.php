8f5e1cd26e7aa3e03553d59e0d7aa6c17637a0e3
Diki Siswanto <dickyachmady@gmail.com>
2022-05-29 10:12:05 +0800

    fix(typo): wrong tag name

--- loop-files.php
+++ loop-files.php
@@ -47,7 +47,7 @@
 
 <main class="container space-y-5 my-5 flex-1">
   <div class="space-y-4">
-    <j1 class="font-heading text-2xl font-black text-title"><span class="fa fa-bar"></span> <?= ucwords($page_title) ?></j1>
+    <h1 class="font-heading text-2xl font-black text-title"><span class="fa fa-bar"></span> <?= ucwords($page_title) ?></h1>
     <table class="table w-full table-auto">
       <thead>
         <tr>
